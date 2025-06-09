1d0a3e4481788cabc3c8c13d71cbd48538f211d2
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-06-09 15:37:50 +0000

    Update 5 Assign

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict"
+function numberDisplay() {
+    let counter = 0
+    let numbers = ""
+    let numbers2 = ""
+    let userNumMin = parseInt(document.getElementById('minNumber').value)
+    let userNumMax = parseInt(document.getElementById('maxNumber').value)
+
+    while (counter <= userNumMin) {
+        numbers = numbers + counter + "<br>"
+        counter = counter +1
+    }
+    while (counter <= userNumMax) {
+        numbers2 = numbers2 + counter + "<br>"
+        counter = counter +1
+    }
+
+    document.getElementById('min').innerHTML = numbers
+    document.getElementById('max').innerHTML = numbers2
+}
